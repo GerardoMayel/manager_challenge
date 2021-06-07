a097cb1540101f81c6b6523162b91bea7e146e5e
Gerardo Mayel Fernández Alamilla <74444550+GerardoMayel@users.noreply.github.com>
2021-06-07 14:01:34 -0500

    Adding SQL Routine with the second test
    In addition to the technical test.

---
+++ pricing_model_gentera_test.sql
@@ -0,0 +1,28 @@
+create database IF NOT EXISTS gentera_data;
+
+use gentera_data;
+
+drop table if exists gentera_data;
+
+create table if NOT EXISTS model_validation (
+
+    model_id integer not null auto_increment primary key,
+    model_name varchar(50),
+    model_metric varchar(50),
+    model_value float(10,4)
+
+);
+
+insert into model_validation (model_name, model_metric, model_value) values 
+("Linear","MAE", 2300.0491),
+("Linear","RMSE", 3857.4777),
+("Linear","R^2 Score", 0.8470),
+("Lasso","MAE", 2343.9817),
+("Lasso","RMSE", 3929.7133),
+("Lasso","R^2 Score", 0.8470),
+("Ridge","MAE", 2302.2564),
+("Ridge","RMSE", 3866.9362),
+("Ridge","R^2 Score", 0.8518)
+;
+
+select * from gentera_data.model_validation;
